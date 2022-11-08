6495726ebddfeec90d589799b000c3905df9c982
kumar162001 <115209389+kumar162001@users.noreply.github.com>
2022-11-08 16:27:39 +0530

    Add files via upload

---
+++ Api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+    method: 'GET',
+    headers: {
+        'X-RapidAPI-Key': 'esGbwrm390mshS2BCl0RALxQRtZTp1w7sFMjsnyJlJzDXVkW0H',
+        'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+    }
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
